cfb8cd3479dcfa58c178956e34c85f990dec542a
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-10-02 14:04:29 -0700

    add functions for giving users secure passwords

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,4 +3,10 @@ require "spec_helper"
 RSpec.describe(User) do
   it { should have_many :reviews }
   it { should have_many :carts }
+
+  it "will create a user with a hashed password" do
+    new_user = User.create({:name => "Javi", :username => "chinchilla", :pass => "iluvchinchillas", :email => "javierrcc522@gmail.com", :is_admin => true})
+
+    expect(new_user.name).to eq("Javi")
+  end
 end
