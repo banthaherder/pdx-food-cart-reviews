46e49d0b3a00dd6eebe4d2ce47a0be174513cfd2
Aaron Rohrbacher <arjazz@icloud.com>
2017-10-02 13:23:16 -0700

    fix class bugs

--- app.rb
+++ app.rb
@@ -5,6 +5,6 @@ enable :sessions
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
-@featured_reviews = Review.all.where("review >= 4").sample(3)
-erb(:index)
+  @featured_reviews = Review.all.where("rating >= 4").sample(3)
+  erb(:index)
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,4 @@
-class User
+class User < ActiveRecord::Base
   has_many :reviews
   has_many :carts, through: :reviews
 end
