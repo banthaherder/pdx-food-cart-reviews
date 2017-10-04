ce37d70e008b2f94984611aa2f5c88445996a83b
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-10-04 13:18:28 -0700

    add lat/lng. Progress on cart search/add.

--- lib/cart.rb
+++ lib/cart.rb
@@ -4,31 +4,39 @@ class Cart < ActiveRecord::Base
   # AIzaSyAaN83hTHVzlAMvkBd4oc3NGFm4YQ-K71I
   # 'AIzaSyDGlceW7yZG05uKfRqWqTHC3mg8Tlfw54w'
 
-  before_save(:get_name, :get_hours, :get_photos, :get_phone_number, :get_address)
+  before_save(:get_name, :get_hours, :get_photos, :get_phone_number, :get_address, :get_lat, :get_lng)
 
   def pic()
     # photos.split(",").sample(1).first.fetch_url(800)
-    self.photos=(GooglePlaces::Client.new('AIzaSyDGlceW7yZG05uKfRqWqTHC3mg8Tlfw54w').spot(self.gp_id).photos.sample(1).first.fetch_url(800))
+    self.photos=(GooglePlaces::Client.new('AIzaSyAaN83hTHVzlAMvkBd4oc3NGFm4YQ-K71I').spot(self.gp_id).photos.sample(1).first.fetch_url(800))
   end
 
 private
   def get_name()
-    self.name=(GooglePlaces::Client.new('AIzaSyDGlceW7yZG05uKfRqWqTHC3mg8Tlfw54w').spot(self.gp_id).name.to_s)
+    self.name=(GooglePlaces::Client.new('AIzaSyAaN83hTHVzlAMvkBd4oc3NGFm4YQ-K71I').spot(self.gp_id).name.to_s)
   end
 
   def get_hours()
-    self.hours=(GooglePlaces::Client.new('AIzaSyDGlceW7yZG05uKfRqWqTHC3mg8Tlfw54w').spot(self.gp_id).opening_hours["weekday_text"])
+    self.hours=(GooglePlaces::Client.new('AIzaSyAaN83hTHVzlAMvkBd4oc3NGFm4YQ-K71I').spot(self.gp_id).opening_hours["weekday_text"])
   end
 
   def get_photos()
-    self.photos=(GooglePlaces::Client.new('AIzaSyDGlceW7yZG05uKfRqWqTHC3mg8Tlfw54w').spot(self.gp_id).photos)
+    self.photos=(GooglePlaces::Client.new('AIzaSyAaN83hTHVzlAMvkBd4oc3NGFm4YQ-K71I').spot(self.gp_id).photos)
   end
 
   def get_phone_number()
-    self.phone_number=(GooglePlaces::Client.new('AIzaSyDGlceW7yZG05uKfRqWqTHC3mg8Tlfw54w').spot(self.gp_id).formatted_phone_number)
+    self.phone_number=(GooglePlaces::Client.new('AIzaSyAaN83hTHVzlAMvkBd4oc3NGFm4YQ-K71I').spot(self.gp_id).formatted_phone_number)
   end
 
   def get_address()
-    self.address=(GooglePlaces::Client.new('AIzaSyDGlceW7yZG05uKfRqWqTHC3mg8Tlfw54w').spot(self.gp_id).formatted_address)
+    self.address=(GooglePlaces::Client.new('AIzaSyAaN83hTHVzlAMvkBd4oc3NGFm4YQ-K71I').spot(self.gp_id).formatted_address)
+  end
+
+  def get_lat()
+    self.lat=(GooglePlaces::Client.new('AIzaSyAaN83hTHVzlAMvkBd4oc3NGFm4YQ-K71I').spot(self.gp_id).lat)
+  end
+
+  def get_lng()
+    self.lng=(GooglePlaces::Client.new('AIzaSyAaN83hTHVzlAMvkBd4oc3NGFm4YQ-K71I').spot(self.gp_id).lng)
   end
 end
