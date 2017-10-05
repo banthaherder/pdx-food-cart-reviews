bedf27ee9a2afb58a0c299e5630b5a6025427b72
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-10-05 10:31:27 -0700

    add a couple backend validations

--- lib/review.rb
+++ lib/review.rb
@@ -1,4 +1,16 @@
 class Review < ActiveRecord::Base
   belongs_to :cart
   belongs_to :user
+
+  validates(:price, {:presence => true})
+  before_save(:format_money, :cap_food_name)
+
+
+private
+  def cap_food_name
+    self.food_name=(food_name().split.map!{|word| word.capitalize}.join(' '))
+  end
+  def format_money
+    self.price=(sprintf('%.2f', price()))
+  end
 end
